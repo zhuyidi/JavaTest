aa69f69c87e3d1f8a115cce5be38d362c2c467f9
zhuyidi <1311243087@qq.com>
2017-09-24 14:03:03 +0800

    update

---
+++ JavaTest/src/ParameterPassingTest/PointTest.java
@@ -0,0 +1,36 @@
+package ParameterPassingTest;
+
+/**
+ * Created by dela on 9/22/17.
+ */
+public class PointTest {
+    private int num1;
+    private int num2;
+
+    public static void main(String[] args){
+        int num1 = 1;
+        int num2 = 2;
+        String str = "111";
+
+        PointTest p1 = new PointTest();
+        p1.change(num1, num2);
+        p1.change(str);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(str);
+    }
+
+    public void change(int num1, int num2){
+        int temp;
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+
+    }
+
+    public void change(String str){
+        str = "222";
+    }
+
+}
